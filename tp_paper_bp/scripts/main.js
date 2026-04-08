2eea3313342e917c90b547e90035ecaf5eca52c1
Fagon <fagon@example.com>
2026-04-09 03:37:41 +0700

    Thêm item_texture.json để trị tàng hình và chèn Debug code

--- tp_paper_bp/scripts/main.js
+++ tp_paper_bp/scripts/main.js
@@ -1,8 +1,14 @@
 import { world, system } from "@minecraft/server";
 
 const TpPaperComponent = {
+    onUse(event) {
+        const player = event.source;
+        player.sendMessage("§e[Debug] Đã nhấn giữ - bắt đầu kích hoạt giấy dịch chuyển...");
+    },
     onCompleteUse(event) {
         const player = event.source;
+        player.sendMessage("§e[Debug] onCompleteUse kích hoạt! Hoàn tất giữ vật phẩm.");
+        
         if (player.typeId !== "minecraft:player") return;
 
         const equipment = player.getComponent("minecraft:equippable");
@@ -28,18 +34,32 @@ const TpPaperComponent = {
                     }
                 }
             }
+            player.sendMessage("§e[Debug] Bước trừ vật phẩm hoàn thành.");
         }
 
-        const spawnPos = player.getSpawnPoint();
+        player.sendMessage("§e[Debug] Đang đọc thông tin giường ngủ...");
+        let spawnPos;
+        try {
+            spawnPos = player.getSpawnPoint();
+        } catch(e) {
+            player.sendMessage("§c[Debug] Lỗi trong lúc lấy spawn point: " + e);
+        }
+
         if (spawnPos) {
-            player.teleport(spawnPos, { dimension: spawnPos.dimension });
-            player.sendMessage("§a[Hệ thống] Đã dịch chuyển bạn về điểm hồi sinh an toàn!");
+            try {
+                player.sendMessage(`§a[Debug] Giường nằm ở: ${spawnPos.x}, ${spawnPos.y}, ${spawnPos.z}`);
+                player.teleport(spawnPos, { dimension: spawnPos.dimension });
+                player.sendMessage("§a[Hệ thống] Đã dịch chuyển bạn về điểm hồi sinh an toàn!");
+            } catch (err) {
+                 player.sendMessage(`§c[Debug] Ngoại lệ khi chạy lệnh teleport: ${err}`);
+            }
         } else {
-            player.sendMessage("§c[Hệ thống] Bạn chưa có điểm hồi sinh nào thiết lập từ giường cả!");
+            player.sendMessage("§c[Hệ thống] Tọa độ giường trống (bằng không). Bạn chưa có điểm hồi sinh thiết lập từ giường!");
         }
     }
 };
 
 world.beforeEvents.worldInitialize.subscribe((initEvent) => {
     initEvent.itemComponentRegistry.registerCustomComponent("custom:tp_paper_use", TpPaperComponent);
+    console.warn("[Debug] Đã đăng ký custom component: custom:tp_paper_use");
 });
